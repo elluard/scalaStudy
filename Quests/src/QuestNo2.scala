f98f6220e02c38cb9c8662f55ec1ba615ef17edb
leehwangchun <elluard@me.com>
2016-06-04 10:59:33 +0900

    #두번째 퀘스트 완료

--- Quests/src/QuestNo2.scala
+++ Quests/src/QuestNo2.scala
@@ -9,12 +9,28 @@ object QuestNo2 {
             false
     }
 
+    def isEven(value : Int) : Boolean = {
+        if(value % 2 == 1)
+            false
+        else
+            true
+    }
+
     def main(args : Array[String]) : Unit = {
         var sum = 0
-        for (
-            i <- 1 until 101
-            if this.isOdd(i)
-        ) sum += i
+        sum = 0
+        for ( i <- 0 to 100)
+            sum += i
+        println(sum)
+
+        sum = 0
+        for ( i <- 0 to 100 if this.isOdd(i) )
+            sum += i
+        println(sum)
+
+        sum = 0
+        for( i <- 0 to 100 if this.isEven(i) )
+            sum += i
         println(sum)
     }
 }
